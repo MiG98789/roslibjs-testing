56322f88e5af4ddb2fab1c3b0be29cd09a382ca5
Gian <gianmiguel98@yahoo.com>
2016-06-30 13:48:23 +0800

    Terminal now shows if value is reset on web page; Added more header files in hpp

--- include/robowebtools/robowebtools.hpp
+++ include/robowebtools/robowebtools.hpp
@@ -1,3 +1,4 @@
 #include "std_msgs/Int64.h"
+#include "std_msgs/UInt8.h"
 #include <iostream>
 #include "ros/ros.h"
--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,27 +1,34 @@
 #include "robowebtools/robowebtools.hpp"
 
+void countCallback(const std_msgs::UInt8::ConstPtr& msg)
+{
+	if(msg->data)
+	{
+		ROS_INFO("Reset count value.");
+	}
+}
+
 int main(int argc, char **argv)
 {  
-  ros::init(argc, argv, "arithmetic_node");
-  ros::NodeHandle n;
-  
-  //ros::Subscriber sub = n.subscribe<std_msgs::UInt8>("/reset", 100, &countCallback);
-  ros::Publisher pub = n.advertise<std_msgs::Int64>("/arithmetic/update", 1);
-  std_msgs::Int64 msg;
-  
-  int  temp = 0;
-  
-  
-  while(ros::ok())
-  {    
-    ros::spinOnce();
-    
-    std::cout << "How much do you want to add? ";
-    std::cin >> temp;
-    
-    msg.data = temp;
-    pub.publish(msg);
-  }
-  
-  return 0;
+	ros::init(argc, argv, "arithmetic_node");
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe<std_msgs::UInt8>("/arithmetic/update/status", 100, &countCallback);
+	ros::Publisher pub = n.advertise<std_msgs::Int64>("/arithmetic/update/value", 1);
+	std_msgs::Int64 msg;
+
+	int  temp = 0;
+
+	while(ros::ok())
+	{    
+		ros::spinOnce();
+
+		std::cout << "How much do you want to add? ";
+		std::cin >> temp;
+
+		msg.data = temp;
+		pub.publish(msg);
+	}
+
+	return 0;
 }
